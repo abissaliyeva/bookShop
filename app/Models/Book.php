ae67cd1fc41aaf7c01904ce48e1d9248e0981d67
Аружан <aruzhan.abissaliyeva@narxoz.kz>
2026-05-06 10:40:13 +0500

    final

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,10 +2,8 @@
 
 namespace App\Models;
 
-use ApiPlatform\Metadata\ApiResource;
 use Illuminate\Database\Eloquent\Model;
 
-#[ApiResource]
 class Book extends Model
 {
     protected $fillable = ['title', 'author', 'price','file_path', 'cover_image'];
